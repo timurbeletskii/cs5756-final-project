f8ec5292e3e7c8981b874dcaac00b3a942a16878
timurbeletskii <tab282@cornell.edu>
2023-05-02 20:41:45 -0400

    Added PPO and A2C models

--- experiments_ppo.sh
+++ experiments_ppo.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# # Default reward setting
-# python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_sh/ppo_1 --log_num 0
+# Default reward setting
+python main.py --algo ppo --lr 0.0003 --reward_type custom --model_path ./ppo_model_checkpoints_md/ppo_1 --log_num 0
 
-# # Experiments on impact of opponent weighting
-# python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_sh/ppo_op_5 --reward_type custom --op_wgt 0.5 --log_num 1
-# python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_sh/ppo_op_1_5 --reward_type custom --op_wgt 1.5 --log_num 2
+# Experiments on impact of opponent weighting
+python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_md/ppo_op_5 --reward_type custom --op_wgt 0.5 --log_num 1
+python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_md/ppo_op_1_5 --reward_type custom --op_wgt 1.5 --log_num 2
 
 # Experiments on giving active pokemon more weight (own team and opponent team) 
-python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_sh/ppo_act_1_5 --reward_type custom --act_wgt 0.5 --log_num 3
+python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_md/ppo_act_1_5 --reward_type custom --act_wgt 0.5 --log_num 3
 
 # HP Shift on active pokemon 
-python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_sh/ppo_shift_5 --reward_type custom --hp_shift 0.5 --log_num 4
+python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_md/ppo_shift_5 --reward_type custom --hp_shift 0.5 --log_num 4
 
 # Best weight
-python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_sh/ppo_best_5 --reward_type custom --op_wgt 1.1 \
+python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_md/ppo_best_5 --reward_type custom --op_wgt 1.1 \
 --act_wgt 1.1 --status_val 0.5 --log_num 5
 
 # Default reward setting
-python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_sh/ppo_6 --log_num 6
+python main.py --algo ppo --lr 0.0003 --model_path ./ppo_model_checkpoints_md/ppo_6 --log_num 6
